88dc00179b124a636ff22460ae72f2dac9146d78
IcecoolR <45355900+IcecoolR@users.noreply.github.com>
2020-09-17 17:19:25 +0100

    Simple Button Click Events
    Began implementing the button click events for creating new goals. The user can also 'delete' the goal as well. This lays the foundations to let users create their own goal objects.

---
+++ Take That 1st Step/Assets/Scripts/MainController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MainController : MonoBehaviour
+{
+
+    [SerializeField]
+    public GameObject Goal1;
+
+    [SerializeField]
+    public Button NewGoal;
+
+
+    // Start is called before the first frame update
+    // void Start() { }
+
+    // Update is called once per frame
+    // void Update() { }
+
+    public void newGoalButtonHandler() {
+        Debug.Log("Goal Created!");
+        Goal1.SetActive(true);
+    }
+
+    public void deleteGoalButtonHandler()
+    {
+        Debug.Log("Goal Deleted!");
+        Goal1.SetActive(false);
+    }
+
+
+}

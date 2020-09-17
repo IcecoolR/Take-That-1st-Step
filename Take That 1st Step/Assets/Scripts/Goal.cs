d83c1fcd8b240b3b72521e374382fd07325c5e82
IcecoolR <45355900+IcecoolR@users.noreply.github.com>
2020-09-17 10:50:26 +0100

    Created Goal.cs and Habit.cs
    Created the object classes required to run the application. Some tweaks were required from the documentation.

---
+++ Take That 1st Step/Assets/Scripts/Goal.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    
+    private string goal;
+    private System.DateTime timeCreated;
+
+    public Goal(string goal) {
+
+        this.goal = goal;
+        this.timeCreated = System.DateTime.Now;
+
+    }
+
+    public string getGoal() {
+        return this.goal;
+    }
+
+}
---
+++ Take That 1st Step/Assets/Scripts/Habit.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Habit : MonoBehaviour
+{
+
+    private string habit;
+    private int daysLeft;
+    private System.DateTime timeCreated;
+
+    public Habit(string habit, int daysLeft) {
+
+        this.habit = habit;
+        this.daysLeft = daysLeft;
+        this.timeCreated = System.DateTime.Now;
+
+    }
+
+    public string getHabit() {
+        return habit;
+    }
+
+    public int getDaysLeft() {
+        return daysLeft;
+    }
+
+    public System.DateTime getTimeCreated() {
+        return timeCreated;
+    }
+
+    public Goal createGoal() {
+        return new Goal(habit);
+    }
+
+}

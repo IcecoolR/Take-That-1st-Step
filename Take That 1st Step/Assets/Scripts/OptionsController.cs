6844b99d7ab3088de38b657a150a77b1422eb9fb
IcecoolR <45355900+IcecoolR@users.noreply.github.com>
2020-09-18 17:06:17 +0100

    Added Information Panel & Button Events
    Began creating the events for the Inspiration, Calendar & Options menu buttons.

---
+++ Take That 1st Step/Assets/Scripts/OptionsController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsController : MonoBehaviour
+{
+    
+    [SerializeField]
+    public GameObject InformationPanel;
+
+
+    public void InspirationButtonHandler() {
+        Debug.Log("Inspiration!");
+        InformationPanel.SetActive(true);
+    }
+
+    public void CalendarButtonHandler() {
+        Debug.Log("Calendar!");
+        InformationPanel.SetActive(true);
+    }
+
+    public void OptionsButtonHandler() {
+        Debug.Log("Options!");
+        InformationPanel.SetActive(true);
+    }
+
+    public void CloseInformationTabButtonHandler() {
+        Debug.Log("Information Panel Closed!");
+        InformationPanel.SetActive(false);
+    }
+}
